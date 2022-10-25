cf80c1a485b871ff29cb4ec1c54fba554bb4d2df
Md Tanzil Hasan Limon <tanzilhasanlimon@gmal.com>
2022-10-25 15:15:01 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZabSidLBeYNPTKTeQHEaZRXNeSwpsGHQ",
+  authDomain: "b610-lerning-platform.firebaseapp.com",
+  projectId: "b610-lerning-platform",
+  storageBucket: "b610-lerning-platform.appspot.com",
+  messagingSenderId: "1002275918345",
+  appId: "1:1002275918345:web:478ee121dd85d307fd5ed8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
